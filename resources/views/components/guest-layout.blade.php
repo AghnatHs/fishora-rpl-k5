6dd907704467097b4455af85de93f5cc2da1c3fc
AghnatHs <aghnathasya@gmail.com>
2025-04-16 00:01:50 +0700

    Feat: Add prevent form submitting handler js

--- resources/views/components/guest-layout.blade.php
+++ resources/views/components/guest-layout.blade.php
@@ -6,7 +6,9 @@
     <title>{{ config('app.name') }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/js/utils/onFormSubmit.js'])
 
     <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"
         integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
