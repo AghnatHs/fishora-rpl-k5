02e4e9317739661411401110d9ee715f1e3583c5
AghnatHs <aghnathasya@gmail.com>
2025-04-15 20:45:29 +0700

    Feat: dismissible errors modal component using alpinejs

--- resources/views/components/modals/errors.blade.php
+++ resources/views/components/modals/errors.blade.php
@@ -1,5 +1,12 @@
 @if ($errors->any())
-    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+    <div x-data="{ open: true }" x-show="open" x-transition
+        class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+        <button @click="open = false" class="absolute top-0 right-0 p-2 text-red-500">
+            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+            </svg>
+        </button>
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
