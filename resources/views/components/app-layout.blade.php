7514b7aed4988382e9e4e84d4a1f05a27cbd6016
AghnatHs <aghnathasya@gmail.com>
2025-04-22 11:53:41 +0700

    Chore: Setup livewire

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -9,6 +9,7 @@
     
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @vite(['resources/js/utils/onFormSubmit.js'])
+    @livewireStyles
 
     <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
     <script src="//unpkg.com/alpinejs" defer></script>
@@ -27,6 +28,8 @@
 
         {{ $slot }}
     </div>
+
+    @livewireScripts
 </body>
 
 </html>
