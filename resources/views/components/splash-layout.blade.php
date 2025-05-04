a8c69f08986d7e78b782f9a70e8e7f05ac205b2e
fauzan-arift <fauzan.ariftricahya@gmail.com>
2025-05-04 14:43:14 +0700

    feat: add splash-layout component for login page

---
+++ resources/views/components/splash-layout.blade.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <title>{{ config('app.name', 'Fishora') }}</title>
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"
+        integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
+</head>
+<body class="bg-gray-100 m-0 p-0">
+    <div class="min-h-screen">
+        {{ $slot }}
+    </div>
+    
+    @livewireScripts
+</body>
+</html>
