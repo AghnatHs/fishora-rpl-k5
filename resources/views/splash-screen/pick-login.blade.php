d20399cd08a6d4f128e86b88ba7f5f38a6ecae5f
fauzan-arift <fauzan.ariftricahya@gmail.com>
2025-05-04 14:45:49 +0700

    feat: implement role selection splash screen

---
+++ resources/views/splash-screen/pick-login.blade.php
@@ -0,0 +1,41 @@
+<x-splash-layout>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text&display=swap" rel="stylesheet">
+    
+    {{-- Splash Screen --}}
+    <div id="splash-screen" class="flex items-center justify-center min-h-screen" style="background-color: #FAFAFA;">
+        <div class="w-full max-w-md mx-auto flex flex-col items-center justify-center">
+            <img src="{{ asset('images/fishora_logo.png') }}" alt="Fishora Logo" alt="Fishora Logo" class="w-[600px] h-[600px] md:w-[700px] md:h-[800px] lg:w-[1546px] lg:h-[1546px] object-contain">
+        </div>
+    </div>
+
+    {{-- Role Selection --}}
+    <div id="role-selection" class="hidden min-h-screen flex items-center justify-center" style="background-color: #FAFAFA;">
+        <div class="w-full max-w-md mx-auto px-4 flex flex-col items-center justify-center">
+            <img src="{{ asset('images/fishora_logo.png') }}" alt="Fishora Logo" class="w-80 md:w-80 h-auto mb-[0px]">
+            
+            
+            <div class="space-y-4 w-full mt-0">
+                <a href="{{ route('customer.login') }}" 
+                   class="block w-5/6 mx-auto font-serif text-[20px] font-normal py-3 px-4 rounded-lg text-center transition duration-200 text-white bg-[#ABCDFF] hover:bg-[#4871AD]">
+                    Log in as Customer
+                </a>
+                <a href="{{ route('seller.login') }}" 
+                   class="block w-5/6 mx-auto font-serif text-[20px] font-normal py-3 px-4 rounded-lg text-center transition duration-200 text-white bg-[#ABCDFF] hover:bg-[#4871AD]">
+                    Log in as Seller
+                </a>
+                <a href="{{ route('admin.login') }}" 
+                   class="block w-5/6 mx-auto font-serif text-[20px] font-normal py-3 px-4 rounded-lg text-center transition duration-200 text-white bg-[#ABCDFF] hover:bg-[#4871AD]">
+                    Log in as Admin
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        // Splash 1,5 detik
+        setTimeout(() => {
+            document.getElementById('splash-screen').classList.add('hidden');
+            document.getElementById('role-selection').classList.remove('hidden');
+        }, 1500);
+    </script>
+</x-splash-layout>
