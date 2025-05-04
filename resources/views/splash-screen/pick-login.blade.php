13c84ca8c18e1054777d3f9f52aac0f014388906
AghnatHs <aghnathasya@gmail.com>
2025-05-04 15:21:27 +0700

    Add defer to avoid render blocking

--- resources/views/splash-screen/pick-login.blade.php
+++ resources/views/splash-screen/pick-login.blade.php
@@ -13,7 +13,6 @@
         <div class="w-full max-w-md mx-auto px-4 flex flex-col items-center justify-center">
             <img src="{{ asset('images/fishora_logo.png') }}" alt="Fishora Logo" class="w-80 md:w-80 h-auto mb-[0px]">
             
-            
             <div class="space-y-4 w-full mt-0">
                 <a href="{{ route('customer.login') }}" 
                    class="block w-5/6 mx-auto font-serif text-[20px] font-normal py-3 px-4 rounded-lg text-center transition duration-200 text-white bg-[#ABCDFF] hover:bg-[#4871AD]">
@@ -31,7 +30,7 @@
         </div>
     </div>
 
-    <script>
+    <script defer>
         // Splash 1,5 detik
         setTimeout(() => {
             document.getElementById('splash-screen').classList.add('hidden');
