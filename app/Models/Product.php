9791f8fd3379e259f8e327cc8ec2bba5b8fc4c22
AghnatHs <aghnathasya@gmail.com>
2025-05-05 13:56:43 +0700

    Fix: Product warning should be 'warnings'

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -39,7 +39,7 @@ class Product extends Model
         return $this->belongsTo(Seller::class);
     }
 
-    public function warning(): HasMany
+    public function warnings(): HasMany
     {
         return $this->hasMany(ProductWarning::class);
     }
