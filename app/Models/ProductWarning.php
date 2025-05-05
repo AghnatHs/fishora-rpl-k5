c06780af1e2ab87feb4b166097f6636f668f6d79
AghnatHs <aghnathasya@gmail.com>
2025-05-05 12:42:10 +0700

    Feat: ProductWarning migration and model

---
+++ app/Models/ProductWarning.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class ProductWarning extends Model
+{
+    use HasUlids;
+
+    protected $fillable = [
+        'description',
+        'status',
+        'price',
+        'product_id',
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (!$model->id) {
+                $model->id = Str::ulid()->toBase32();
+            }
+        });
+    }
+}
