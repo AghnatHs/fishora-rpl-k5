2d6bd98a9b95258da8704badeec11a709df2d9a4
AghnatHs <aghnathasya@gmail.com>
2025-05-07 23:50:14 +0700

    Fix: Unnecessary back button in verify-email-notice

--- resources/views/customer/auth/verify-email-notice.blade.php
+++ resources/views/customer/auth/verify-email-notice.blade.php
@@ -3,11 +3,6 @@
     {{-- Sticky Navbar with buttons at screen edges --}}
     <div class="fixed top-0 left-0 right-0 bg-white z-50">
         <div class="flex justify-between items-center px-4 sm:px-6 py-3 w-full">
-            <a href="{{ route('pick-login') }}" class="text-[#4871AD] hover:text-[#ABCDFF] transition-colors duration-200">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-                </svg>
-            </a>
             <a href="#" class="text-[#4871AD] hover:text-[#ABCDFF] transition-colors duration-200">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
