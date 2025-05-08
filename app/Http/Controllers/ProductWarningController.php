e57bca4d52a223a93eab5134bffc9e5ec4276963
AghnatHs <aghnathasya@gmail.com>
2025-05-08 23:08:03 +0700

    Feat: Notify Seller when their product is deleted by Admin

--- app/Http/Controllers/ProductWarningController.php
+++ app/Http/Controllers/ProductWarningController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Models\ProductWarning;
 use App\Notifications\ProductWarningNotification;
+use App\Notifications\ProductWarningProductDeletedNotification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -19,8 +20,22 @@ class ProductWarningController extends Controller
             return ['url' => Storage::url($img->filepath)];
         });
         $productImages = collect([$coverImage])->merge($galleryImages);
-        
+
         return view("admin.dashboard.monitoring-create", compact('product', 'productImages'));
+    }
+
+    public function destroy(Product $product)
+    {
+        foreach ($product->images as $image) {
+            Storage::disk('public')->delete($image->filepath);
+            $image->delete();
+        }
+        $seller = $product->seller;
+        $product->delete();
+
+        $seller->notify(new ProductWarningProductDeletedNotification("$product->name deleted by Admin, ignored warning too long."));
+
+        return redirect()->route('admin.dashboard.products-monitoring')->with('success', 'Product deleted successfully!');
     }
 
     public function store(Request $request, Product $product)
