c7c53b109a8b050ba5128b85cdac915e1b71c7f6
AghnatHs <aghnathasya@gmail.com>
2025-05-08 23:23:44 +0700

    Fix(SellerDashboardController): notifications didnt passed to view

--- app/Http/Controllers/SellerDashboardController.php
+++ app/Http/Controllers/SellerDashboardController.php
@@ -14,7 +14,7 @@ class SellerDashboardController extends Controller
             ->orderBy('created_at', 'desc')
             ->get();
         $notifications = Auth::guard('seller')->user()->unreadNotifications;
-        return view('seller.dashboard.index', compact('products'));
+        return view('seller.dashboard.index', compact('products', 'notifications'));
     }
 
     public function inbox()
