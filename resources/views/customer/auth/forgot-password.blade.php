1c9f52441410815e42ed40cef5b891b08b1d6862
AghnatHs <aghnathasya@gmail.com>
2025-05-25 20:41:57 +0700

    Fix: unnecessary double <form>

--- resources/views/customer/auth/forgot-password.blade.php
+++ resources/views/customer/auth/forgot-password.blade.php
@@ -27,26 +27,25 @@
         <h2 class="text-3xl font-medium text-center mb-8" style="color: #4871AD; font-family: 'DM Serif Text', serif;">
             Request Reset Password (Pembeli)</h2>
         <form method="POST" action="{{ route('customer.password.send-email') }}" class="space-y-6 w-full">
-            <form method="POST" action="{{ route('customer.password.send-email') }}">
 
-                @csrf
-                <div>
-                    <input type="email" name="email" id="email" placeholder="Email" required
-                        class="mt-1 block w-full rounded-lg border border-[#4871AD] shadow-sm px-3 py-2"
-                        style="--tw-ring-color: rgba(72, 113, 173, 0.5); --tw-ring-opacity: 0.5; --tw-border-opacity: 1; --focus-border-color: #4871AD;font-family: 'DM Serif Text', serif; color: #9E9595; font-size: 1.2rem;"
-                        onfocus="this.style.borderColor='#4871AD'; this.style.boxShadow='0 0 0 3px rgba(72, 113, 173, 0.25)';"
-                        onblur="this.style.boxShadow='';">
-                </div>
+            @csrf
+            <div>
+                <input type="email" name="email" id="email" placeholder="Email" required
+                    class="mt-1 block w-full rounded-lg border border-[#4871AD] shadow-sm px-3 py-2"
+                    style="--tw-ring-color: rgba(72, 113, 173, 0.5); --tw-ring-opacity: 0.5; --tw-border-opacity: 1; --focus-border-color: #4871AD;font-family: 'DM Serif Text', serif; color: #9E9595; font-size: 1.2rem;"
+                    onfocus="this.style.borderColor='#4871AD'; this.style.boxShadow='0 0 0 3px rgba(72, 113, 173, 0.25)';"
+                    onblur="this.style.boxShadow='';">
+            </div>
 
-                {{-- Submit Button --}}
-                <div>
-                    <button type="submit"
-                        class="w-full text-white font-medium text-xl py-2 px-3 rounded-lg uppercase bg-[#4871AD] hover:bg-[#ABCDFF] transition-colors duration-200"
-                        style="font-family: 'DM Serif Text', serif;">
-                        Kirim Link Reset Password
-                    </button>
-                </div>
-            </form>
+            {{-- Submit Button --}}
+            <div>
+                <button type="submit"
+                    class="w-full text-white font-medium text-xl py-2 px-3 rounded-lg uppercase bg-[#4871AD] hover:bg-[#ABCDFF] transition-colors duration-200"
+                    style="font-family: 'DM Serif Text', serif;">
+                    Kirim Link Reset Password
+                </button>
+            </div>
+
         </form>
     </div>
 </x-app-layout>
